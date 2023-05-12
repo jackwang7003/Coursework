0bea0ed66af285078f8de7386ecd255842cffcb7
jackwang7003 <jackwang7003@gmail.com>
2023-05-12 07:45:25 -0500

    DOM Practice

---
+++ block18/test.js
@@ -0,0 +1,10 @@
+//slice a sring from index 2 to 4 without using slice method
+
+var str = "Hello World!";
+var res = str.substring(2, 4);
+console.log(res);
+
+//convert a string into an array
+var str = "a,b,c,d,e,f";
+var arr = str.split("");
+console.log(arr);
